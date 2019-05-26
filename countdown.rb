58b38b77a543ca688772f2c8ac9ca6a4d0329c44
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-26 04:38:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  loop do
+    puts "#{num} SECOND(S)!"
+    sleep (1) 
+    num -= 1 
+    break if num == 0 
+  end
+  puts HAPPY NEW YEAR!
 end
