cc8cf5bd30ec119e8fcca617283b090349ba022c
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-05-26 04:39:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(num)
-  loop do
+  while num > 0
     puts "#{num} SECOND(S)!"
     sleep (1) 
     num -= 1 
-    break if num == 0 
   end
-  puts HAPPY NEW YEAR!
+  puts "HAPPY NEW YEAR!"
 end
